66bca2cac3793d487eeca08a3295a4c253020b66
mikitakezawa <mikiaaamiki@yahoo.co.jp>
2023-06-15 09:29:40 +0900

    picture view作成しました

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,13 +1,53 @@
 class PicturesController < ApplicationController
   def index
+      @pictures = Picture.all
   end
 
   def new
+      @picture = Picture.new
+  end
+  
+  def create
+      @picture = Picture.new(picture_params)
+      if @picture.save
+          redirect_to pictures_path,notice: 'pictureの投稿に成功しました'
+      else
+          render :new
+      end
   end
 
   def show
+      @picture = Picture.find(params[:id])
   end
 
   def edit
+      @picture = Picture.find(params[:id])
+  end
+  
+  def update
+      @picture = Picture.find(params[:id])
+      if @picture.update(picture_params)
+          redirect_to pictures_path,notice: 'pictureを編集しました'
+      else
+          render :edit
+      end
+  end
+  
+  def destroy
+      @picture = Picture.find(params[:id])
+      @picture.destroy
+      redirect_to pictures_path,notice: 'pictureを削除しました'
+  end
+  
+  def confirm
+      @picture = Picture.new(picture_params)
+      render :new if @picture.invalid?
+  end
+  
+  
+  private
+  
+  def picture_params
+      params.require(:picture).permit(:image,:image_cache,:content)
   end
 end
